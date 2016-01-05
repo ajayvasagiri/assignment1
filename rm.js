290996257881262f6c9dfc117c0b9e9dbb719b45
Ajaykartheek Vasagiri <AVasagiri@walmart.com>
2016-01-05 13:34:39 -0800

    implemented rm

--- rm.js
+++ rm.js
@@ -1,13 +1,34 @@
-#!/usr/bin/env node
+#!/usr/bin/env babel-node
 
 "use strict";
 
 let fs = require('pn/fs')
+let path = require('path')
+let _ = require('lodash')
 
-if (process.argv.length != 3) {
-        console.error('Usage: rm.js <file name>');
-        process.exit(1);
+var rootPath = process.argv[2]
+
+ async function rm(rootPath)
+{
+     try 
+     {
+        let lspromises = []
+        let data = await fs.stat(rootPath)
+        if(data.isFile()){
+           console.log("removing file"+rootPath)
+            let data = await fs.unlink(rootPath)
+            console.log("removing file"+rootPath)
+        }
+        else{
+        	 let fileNames =await fs.readdir(rootPath)
+             for (let fileName of fileNames) 
+             {
+                let filePath = path.join(rootPath, fileName)
+                await rm(filePath)
+            }
+              fs.rmdir(rootPath).then(console.log("removing directory "+rootPath))
+        }
+    } catch (e) {
+        console.log(e.stack)
+    }
 }
-
-fs.rmdir(process.argv[2])
-
